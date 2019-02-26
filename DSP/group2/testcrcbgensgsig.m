f5f0549ab6085f481f285166d4a7e38a1db80281
bingining <bingining@gmail.com>
2019-02-26 17:07:44 +0800

    all the 7 signals worked out by group2

---
+++ DSP/group2/testcrcbgensgsig.m
@@ -0,0 +1,40 @@
+%% Plot the sine-gaussian signal
+% Signal parameters
+a1=10;
+a2=3;
+a3=3;
+a4=2;
+A = 10;
+% Instantaneous frequency after 1 sec is 
+Freq = a3;
+samplFreq = 5*Freq;
+samplIntrvl = 1/samplFreq;
+
+% Time samples
+timeVec = 0:samplIntrvl:1.0;
+% Number of samples
+nSamples = length(timeVec);
+
+% Generate the signal
+sigVec = crcbgensgsig(timeVec,A,[a1,a2,a3,a4]);
+
+% Plot the signal 
+figure;
+plot(timeVec,sigVec)
+
+% Plot the periodogram
+%--------------
+% Length of data 
+dataLen = timeVec(end)-timeVec(1);
+% DFT sample corresponding to Nyquist frequency
+kNyq = floor(nSamples/2)+1;
+% Positive Fourier frequencies
+posFreq = (0:(kNyq-1))*(1/dataLen);
+% FFT of signal
+fftSig = fft(sigVec);
+% Discard negative frequencies
+fftSig = fftSig(1:kNyq);
+
+% Plot periodogram
+figure;
+plot(posFreq,abs(fftSig));
