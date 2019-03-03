e1f514961d3716fa585789c1612ce8ad11b9ffc2
Xiongying Qiao <qiaoxiongying@hotmail.com>
2019-03-03 15:29:16 +0800

    Generating WGN

--- NOISE/group5/WGN.m
+++ NOISE/group5/WGN.m
@@ -34,6 +34,41 @@ yy=hist(y,x);
 yy=yy/length(y);
 figure(2)
 bar(x,yy)%Draw a probability density distribution histogram
+%% WGN with \mu=0, \sigma=sqrt(2)
+sample=10000;
+mu=0;
+sigma=sqrt(2);
+y=mu + sigma* randn(1,sample); 
+figure(1);
+plot(y)
+ymin=min(y);
+ymax=max(y);
+%Divide the maximum and minimum intervals
+%into 20 equal points (19 equal parts), and then
+%calculate the number of each interval separately.
+x=linspace(ymin,ymax,20);
+yy=hist(y,x);
+yy=yy/length(y);
+figure(2)
+bar(x,yy)%Draw a probability density distribution histogram
+%% WGN with \mu=0, \sigma=sqrt(2)
+sample=10000;
+mu=0;
+sigma=sqrt(2);
+y=mu + sigma* randn(1,sample); 
+figure(1);
+plot(y)
+ymin=min(y);
+ymax=max(y);
+%Divide the maximum and minimum intervals
+%into 20 equal points (19 equal parts), and then
+%calculate the number of each interval separately.
+x=linspace(ymin,ymax,20);
+yy=hist(y,x);
+yy=yy/length(y);
+figure(2)
+bar(x,yy)%Draw a probability density distribution histogram
+
 %% WGN with \mu=2, \sigma=sqrt(2)
 sample=10000;
 mu=2;
@@ -51,4 +86,3 @@ yy=hist(y,x);
 yy=yy/length(y);
 figure(2)
 bar(x,yy)%Draw a probability density distribution histogram
-sqrt(2)
