508b445884d33386654ba731951202ee45bab8f5
Xiongying Qiao <qiaoxiongying@hotmail.com>
2019-03-06 00:48:26 +0800

    lab top 4

---
+++ DETEST/group5/ex2.m
@@ -0,0 +1,88 @@
+%%
+% We will reuse codes that have already been written.
+% Path to folder containing signal and noise generation codes
+addpath ../
+addpath ../../DSP/group5/lab1
+addpath ../../NOISE/
+%%
+% This is the target SNR
+snr = 10;
+
+%%
+% Data generation parameters
+nSamples = 2048;
+sampFreq = 1024;
+timeVec = (0:(nSamples-1))/sampFreq;
+
+
+%%
+% Generate the signal that is to be normalized
+A=10;
+f0=20;
+f1=50/3;
+phi=7/5;
+sigVec =Am(A,[f0,f1],phi,timeVec);
+
+%%
+% We will use the noise PSD used in colGaussNoiseDemo.m but add a constant
+% to remove the parts that are zero. (Exercise: Prove that if the noise PSD
+% is zero at some frequencies but the signal added to the noise is not,
+% then one can create a detection statistic with infinite SNR.)
+noisePSD = @(f) (f>=100 & f<=300).*(f-100).*(300-f)/10000 + 1;
+
+%%
+% Generate the PSD vector to be used in the normalization. Should be
+% generated for all positive DFT frequencies. 
+dataLen = nSamples/sampFreq;
+kNyq = floor(nSamples/2)+1;
+posFreq = (0:(kNyq-1))*(1/dataLen);
+psdPosFreq = noisePSD(posFreq);
+figure;
+plot(posFreq,psdPosFreq);
+axis([0,posFreq(end),0,max(psdPosFreq)]);
+xlabel('Frequency (Hz)');
+ylabel('PSD ((data unit)^2/Hz)');
+
+%% Calculation of the norm
+% Norm of signal squared is inner product of signal with itself
+[normSigVec,normFac]=ex1_normSigVec(sigVec,sampFreq,psdPosFreq,snr);% Normalize signal to specified SNR
+%% Test
+%Obtain LLR values for multiple noise realizations
+nH0Data = 1000;
+llrH0 = zeros(1,nH0Data);
+for lp = 1:nH0Data
+    noiseVec = statgaussnoisegen(nSamples,[posFreq(:),psdPosFreq(:)],100,sampFreq);
+    llrH0(lp) = innerprodpsd(noiseVec,sigVec,sampFreq,psdPosFreq);
+end
+%Obtain LLR for multiple data (=signal+noise) realizations
+nH1Data = 1000;
+llrH1 = zeros(1,nH1Data);
+for lp = 1:nH0Data
+    noiseVec = statgaussnoisegen(nSamples,[posFreq(:),psdPosFreq(:)],100,sampFreq);
+    % Add normalized signal
+    dataVec = noiseVec + sigVec;
+    llrH1(lp) = innerprodpsd(dataVec,sigVec,sampFreq,psdPosFreq);
+end
+%% Time domain
+figure;
+hold on;
+plot(timeVec,noiseVec);
+plot(timeVec,normSigVec);
+xlabel('Time(s)');
+ylabel(['Noise+GaussianChirp'] );
+
+%% Frequency domain
+fftsig=fft(noiseVec+normSigVec);
+L=length(noiseVec);
+figure;
+plot(sampFreq*(0:L/2)/L,abs(fftsig(1:L/2+1)));
+xlabel('Frequency(Hz)');
+ylabel('Noise+ SineGaussian signal');
+
+
+%% Spectrogram
+
+figure;
+spectrogram(noiseVec+normSigVec, 128,127,[],sampFreq);
+
+
